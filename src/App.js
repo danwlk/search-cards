767b39d85c92c2fbd39b63a1a4d4afc9a3ea81b3
Doyup Kim <d25kim@uwaterloo.ca>
2024-03-12 23:50:16 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Search from './Components/Search/Search';
+import { Container, Col, Row, Card } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activeCards, setActiveCards] = useState(4);
+
+    return (
+        <div>
+            <Container fluid>
+                <Row>
+                    <Col md={12 / activeCards}>
+                        <Card className="search-box">
+                            <Search
+                                title="Switch Games"
+                                url="https://api.sampleapis.com/switch/games"
+                            />
+                        </Card>
+                    </Col>
+                    <Col md={12 / activeCards}>
+                        <Card className="search-box">
+                            <Search
+                                title="Coffee"
+                                url="https://api.sampleapis.com/coffee/hot"
+                            />
+                        </Card>
+                    </Col>
+                    <Col md={12 / activeCards}>
+                        <Card className="search-box">
+                            <Search
+                                title="Coding Resources"
+                                url="https://api.sampleapis.com/codingresources/codingResources"
+                            />
+                        </Card>
+                    </Col>
+                    <Col md={12 / activeCards}>
+                        <Card className="search-box">
+                            <Search
+                                title="Beers"
+                                url="https://api.sampleapis.com/beers/ale"
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Search.css';
+import { useState } from 'react';
+
+function Search({ title, url }) {
+    const [data, setData] = useState([]);
+    const [showData, setShowData] = useState(false);
+    const [dataText, setDataText] = useState('Press Button to Fetch Data');
+
+    const getData = () => {
+        setShowData(false);
+        setDataText('Loading...');
+        fetch(url)
+            .then((res) => res.json())
+            .then((json) => {
+                setData(json);
+                setDataText('');
+                setShowData(true);
+            });
+    };
+
+    const clearData = () => {
+        setShowData(false);
+        setData([]);
+        setDataText('Data has been cleared');
+    };
+
+    return (
+        <div>
+            <button>HIDE</button>
+            <h1>{title}</h1>
+            <button className="search-btn" onClick={() => getData()}>
+                {data.length === 0 ? 'Fetch Data' : 'Refresh Data'}
+            </button>
+            <button className="search-btn" onClick={() => clearData()}>
+                Clear
+            </button>
+            <br />
+            <select className='dropdown'>
+                <option value="none">Filter By</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+            </select>
+            <input className='input' type="text"></input>
+            <button className="search-btn">Clear</button>
+            <h3>Data:</h3>
+            {showData ? (
+                <pre>{JSON.stringify(data, null, 4)}</pre>
+            ) : (
+                <div>{dataText}</div>
+            )}
+        </div>
+    );
+}
+
+export default Search;
